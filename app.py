1f8d9ffe110454255c88829caa4a7b2ecb3bcecf
“Pratapj8” <pratap.jadhav0@gmail.com>
2025-04-16 21:11:55 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -8,11 +8,12 @@
 from src.Students_Score_Project.logger import logging
 from src.Students_Score_Project.exception import CustomException
 from src.Students_Score_Project.components.data_ingestion import DataIngestion
-# from src.Students_Score_Project.components.data_ingestion import DataIngestionConfig
+from src.Students_Score_Project.components.data_ingestion import DataIngestionConfig
 from src.Students_Score_Project.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.Students_Score_Project.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 
-
+import os
 import sys
 
 
@@ -28,7 +29,13 @@ if __name__=="__main__": # Project entry point
         
         data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path ,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path ,test_data_path)
+        # _ means skip now
+        
+        ## Model Training
+        
+        maodel_trainer = ModelTrainer()
+        print(maodel_trainer.initiate_model_trainer(train_arr,test_arr))
         
     except Exception as e: # Capture error message
         logging.info("Custom Exception")
